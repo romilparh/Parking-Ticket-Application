403ea2428c5f2eeab26085d1ab7319ce6fa8fd49
Romil <romilparhwal007@gmail.com>
2018-04-18 01:22:12 -0400

    Add Validation for email

---
+++ app/src/main/java/com/romilparh/shadybond/shadyparkingsystem/validation/EMailValidator.java
@@ -0,0 +1,22 @@
+package com.romilparh.shadybond.shadyparkingsystem.validation;
+
+/**
+ * Created by shadybond on 2018-04-17.
+ */
+
+public class EMailValidator {
+
+    public boolean isValidEmailAddress(String email){
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+
+        if(!m.matches()) {
+            return false;
+        } else {
+            return m.matches();
+        }
+
+    }
+
+    }
